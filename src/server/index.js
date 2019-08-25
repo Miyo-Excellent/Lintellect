e9608f92f86e2cb6a472f035dce0114bc3a0a32d
Michell Excellent Marin <theofficesmichell@gmail.com>
2019-08-24 20:42:24 -0500

    update

--- src/server/index.js
+++ src/server/index.js
@@ -53,6 +53,7 @@ cloudinary.v2.config(serverConfig.cloudinary);
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /*****************************************Environment*********************************************/
 const isDevelopment = process.env.NODE_ENV !== 'production';
+
 const isOpenBrowser = process.env.OPEN_BROWSER === 'true';
 
 /*******************************************Analyzer**********************************************/
@@ -70,145 +71,275 @@ const port = process.env.NODE_PORT || 3000;
 /***************************************Apollo Server*******************************************/
 const apolloServer = new ApolloServer({typeDefs, resolvers});
 
-apolloServer.applyMiddleware({app});
+try {
+  apolloServer.applyMiddleware({app});
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /****************************GZip Compression just for Production********************************/
 if (!isDevelopment) {
-  app.get('*.js', (req, res, next) => {
-    req.url = `${req.url}.gz`;
-    res.set('Content-Encoding', 'gzip');
+  try {
+    app.get('*.js', (req, res, next) => {
+      req.url = `${req.url}.gz`;
+      res.set('Content-Encoding', 'gzip');
 
-    next();
-  });
+      next();
+    });
+  } catch (error) {
+    logger.error(error);
+    console.log(error);
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /****************************************Public static********************************************/
-app.use(express.static(path.join(__dirname, '../../public')));
+try {
+  app.use(express.static(path.join(__dirname, '../../public')));
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 /*****************************************Body Parser********************************************/
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+try {
+  app.use(bodyParser.urlencoded({extended: false}));
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 /***********************************parse application/json***************************************/
-app.use(bodyParser.json());
+try {
+  app.use(bodyParser.json());
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 /****************************************Morgan Logger********************************************/
-app.use(morgan('combined'));
+try {
+  app.use(morgan('combined'));
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 /**************************************Device Detection******************************************/
-app.use((req, res, next) => {
-  req.isBot = isBot(req.headers['user-agent']);
-  req.isMobile = isMobile(req.headers['user-agent']);
+try {
+  app.use((req, res, next) => {
+    req.isBot = isBot(req.headers['user-agent']);
+    req.isMobile = isMobile(req.headers['user-agent']);
 
-  return next();
-});
+    return next();
+  });
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 /***************************************API Middleware*******************************************/
-app.use('/api', api);
+try {
+  app.use('/api', api);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /****************************************HTTP Methods********************************************/
 
 //  Initial
-app.use('/', (req, res, next) => {
-  //  logger.info(`IN APP [IP: ${req.ip}] Han ingresado al sitio WEB`);
-  //  console.info(`IN APP [IP: ${req.ip}] Han ingresado al sitio WEB`);
-  return next();
-});
+try {
+  app.use('/', (req, res, next) => {
+    //  logger.info(`IN APP [IP: ${req.ip}] Han ingresado al sitio WEB`);
+    //  console.info(`IN APP [IP: ${req.ip}] Han ingresado al sitio WEB`);
+    return next();
+  });
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 //  Graphql
-app.use('/graphql', graphqlHTTP(serverConfig.graphqlOptions));
+try {
+  app.use('/graphql', graphqlHTTP(serverConfig.graphqlOptions));
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 //  Users
-app.post('/signin', signIn);
-app.post('/signin-with-google', signInWithGoogle);
-app.post('/signup', signUp);
+try {
+  app.post('/signin', signIn);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+try {
+  app.post('/signin-with-google', signInWithGoogle);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+try {
+  app.post('/signup', signUp);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 //  Products
-app.get('/api/products', isAuth, getProducts);
-app.get('/api/product/:productId', isAuth, getProduct);
-app.post('/api/product', isAuth, saveProduct);
-app.put('/api/product/:productId', isAuth, updateProducts);
-app.delete('/api/product/:productId', isAuth, deleteProducts);
+try {
+  app.get('/api/products', isAuth, getProducts);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+
+try {
+  app.get('/api/product/:productId', isAuth, getProduct);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+
+try {
+  app.post('/api/product', isAuth, saveProduct);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+
+try {
+  app.put('/api/product/:productId', isAuth, updateProducts);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
+
+try {
+  app.delete('/api/product/:productId', isAuth, deleteProducts);
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************Hot Module Replacement***************************************/
 if (isDevelopment) {
-  app.use(webpackDevMiddleware(compiler));
-  app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')));
+  try {
+    app.use(webpackDevMiddleware(compiler));
+  } catch (error) {
+    logger.error(error);
+    console.log(error);
+  }
+
+  try {
+    app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')));
+  } catch (error) {
+    logger.error(error);
+    console.log(error);
+  }
 }
 
 /***********************************Client Side Rendering***************************************/
-app.use(clientRender());
+try {
+  app.use(clientRender());
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 if (!isDevelopment) {
   try {
     const serverRender = require('../../dist/server').default;
 
-    app.use(serverRender());
+    try {
+      app.use(serverRender());
+    } catch (error) {
+      logger.error(error);
+      console.log(error);
+    }
   } catch (e) {
     throw e;
   }
 }
 
 /***********************For Server Side Rendering on Development Mode***************************/
-app.use(webpackHotServerMiddleware(compiler));
+try {
+  app.use(webpackHotServerMiddleware(compiler));
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /************************************Data Base Connection****************************************/
-mongoose.connect(serverConfig.db, err => {
-  if (err) {
-    logger.info(`Error al conectar a la base de datos ${err}`);
-    console.info(`Error al conectar a la base de datos ${err}`);
-  } else {
-    logger.info('Conexión a la Base de Datos establecida');
-    console.info('Conexión a la Base de Datos establecida');
-  }
+try {
+  mongoose.connect(serverConfig.db, err => {
+    if (err) {
+      logger.info(`Error al conectar a la base de datos ${err}`);
+      console.info(`Error al conectar a la base de datos ${err}`);
+    } else {
+      logger.info('Conexión a la Base de Datos establecida');
+      console.info('Conexión a la Base de Datos establecida');
+    }
 
-  // Listening
-  app.listen(port, err => {
-    if (!err && !isAnalyzer) {
-      const messages = {
-        app: `
+    // Listening
+    try {
+      app.listen(port, err => {
+        if (!err && !isAnalyzer) {
+          const messages = {
+            app: `
         ============================================================================================================================
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         |·· Aplicación corriendo en: ==>  http://localhost:${port}  <== Abrir enlace con (Ctrl + Clic) en Windows, Linux, MAC ·|
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         ============================================================================================================================
       `,
-        graphql: `
+            graphql: `
         ============================================================================================================================
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         |· GraphQL Server corriendo en: ==>  http://localhost:${port}/graphql  <== Abrir enlace con (Ctrl + Clic) en Windows, Linux, MAC ·|
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         ============================================================================================================================
       `,
-        apollo: `
+            apollo: `
         ============================================================================================================================
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         |· Apollo Server corriendo en: ==>   http://localhost:${port}/apollo${apolloServer.graphqlPath}  <== Abrir enlace con (Ctrl + Clic) en Windows, Linux, MAC ·|
         |·─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─··─·─··─··─·|
         ============================================================================================================================
         `
-      };
+          };
 
 
-      logger.info(messages.app);
-      console.info(messages.app);
+          logger.info(messages.app);
+          console.info(messages.app);
 
-      logger.info(messages.graphql);
-      console.info(messages.graphql);
+          logger.info(messages.graphql);
+          console.info(messages.graphql);
 
-      logger.info(messages.apollo);
-      console.info(messages.apollo);
+          logger.info(messages.apollo);
+          console.info(messages.apollo);
 
-      if (isOpenBrowser) {
-        open(`http://localhost:${port}`);
-      }
+          if (isOpenBrowser) {
+            open(`http://localhost:${port}`);
+          }
+        }
+      });
+    } catch (error) {
+      logger.error(error);
+      console.log(error);
     }
   });
-});
+} catch (error) {
+  logger.error(error);
+  console.log(error);
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
